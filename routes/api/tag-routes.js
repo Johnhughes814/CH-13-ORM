57e82bf772a1be0b3bf1f2b75207b8a7367fd281
Johnhughes814 <8hughes.john@gmail.com>
2022-04-14 12:35:06 -0500

    tag-routes.js GET route

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -5,7 +5,24 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 router.get('/', (req, res) => {
   // find all tags
+  Tag.findAll ({
   // be sure to include its associated Product data
+      include: {
+        model: Product,
+        attributes: ['id', 'product_name', 'price', 'stock', 'category_id']
+      }
+    })
+    .then(TagDB => {
+      if (!TagDB) {
+        res.status(404).json({ message: "No tags found!" });
+        return;
+      } 
+      res.json(TagDB);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 router.get('/:id', (req, res) => {
